d5a123408b9f139024732247b5729cf46f98ca0d
pramim7antofaz@gmail.com <pramim7antofaz@gmail.com>
2022-10-03 21:50:32 -0300

    Js semi finalizado

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-document.querySelector("#file-selector").addEventListener("change", pegarData);
+//let file = document.querySelector("#file-selector").addEventListener("change", pegarData);
 
-let moedas = document.querySelector("#moedas");
-let chaves = document.querySelector("#chaves");
-let snowboards = document.querySelector("#snowboards");
-let bVerm = document.querySelector("#bVerm");
-let bAzul = document.querySelector("#bAzul");
-let inputs = [];
+function pegarData(){
+  let moedas = document.querySelector("#moedas").value;
+  let chaves = document.querySelector("#chaves").value;
+  let snowboards = document.querySelector("#snowboards").value;
+  let bVerm = document.querySelector("#bVerm").value;
+  let bAzul = document.querySelector("#bAzul").value;
+  let inputs = [moedas, chaves, snowboards, bVerm, bAzul];
 
-function pegarData(event){
-  const file = document.querySelector("#file-selector").files[0];
+  const file = document.querySelector("#file-selector").files[0]; // first file in files list
   if (file){
-    //document.querySelector("p").innerHTML = file;
-    console.log(file);
     var reader = new FileReader();
     reader.readAsText(file, "UTF-8");
     reader.onload = (event) =>{
-    alterarData(event.target.result);
+      let file_content = event.target.result;
+    for (let item = 0; item < inputs.length; item++){
+      file_content = alterarData(file_content, item, inputs[item])
+    }
+    document.querySelector('textarea').innerText = file_content
+      /*console.log(item);
+      console.log(file_content)
+      document.querySelector('textarea').innerText = file_content;*/
     }
   }
 }
 
-function alterarData(data){
-  let id = 1;
-  let money = 22;
-  let patt = /\\"1\\":{\\"value\\":[0-9]*/g;
-  console.log(patt);
-  console.log(data.search(patt));
-  data = data.replace(patt, /\\"1":{"value":/ + money);
-}
-
-function bla(){
-  console.log(moedas.value);
+function alterarData(data, id, valor){
+  //let patt = new RegExp(`\\\"${id}\\\":{\\\"value\\\":[0-9]*`);
+  let patt = RegExp(`/\\"${id}\\":{\\"value\\":[0-9]*/`);
+  let newPatt = `\\"${id}\\":{\\"value\\":${valor}`;
+  console.log(data.search(patt))
+  return data.replace(patt, newPatt);
 }
 /*let p = document.querySelector("p");
 p.innerHTML = '{"version":3,"data":"{\"lastSaved\":\"2022-08-29T10:17:24.172974Z\",\"patchVersion\":0,\"currencies\":{\"3\":{\"value\":10,\"expirationType\":0},\"2\":{\"value\":5,\"expirationType\":0},\"4\":{\"value\":5,\"expirationType\":0},\"5\":{\"value\":5,\"expirationType\":0},\"1\":{\"value\":10000,\"expirationType\":0}},\"lootboxQueue\":{\"unopenedLootboxes\":[]},\"currencyAllowedInRun\":{},\"lootBoxesOpened\":{},\"ownedOnlyBuyOnceProducts\":[]}"}';
